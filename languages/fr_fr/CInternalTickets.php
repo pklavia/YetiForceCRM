2954c51d7f07ca201984900bf038ebe7d32e002f
pklavia <pklavia@mail.com>
2017-02-10 17:32:19 +0200

    Translated into French

--- languages/fr_fr/CInternalTickets.php
+++ languages/fr_fr/CInternalTickets.php
@@ -1,25 +1,25 @@
 <?php
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 $languageStrings = [
-	'CInternalTickets' => 'Internal tickets',
-	'SINGLE_CInternalTickets' => 'Internal ticket',
+	'CInternalTickets' => 'Tickets internes',
+	'SINGLE_CInternalTickets' => 'Ticket interne',
 	//BLOCKS
-	'LBL_ADDITIONAL_INFORMATION' => 'Additional information',
-	'LBL_BASIC_INFORMATION' => 'Basic information',
+	'LBL_ADDITIONAL_INFORMATION' => 'Information supplémentaire',
+	'LBL_BASIC_INFORMATION' => 'Information de base',
 	'LBL_TICKET_RESOLUTION' => 'Solution',
 	'LBL_ATTENTION_BLOCK' => 'Notes',
 	'LBL_DESCRIPTION_BLOCK' => 'Description',
 	//FIELDS
-	'FL_SUBJECT' => 'Subject',
-	'FL_STATUS' => 'Status',
+	'FL_SUBJECT' => 'Sujet',
+	'FL_STATUS' => 'Statut',
 	'FL_RESOLUTION' => 'Solution',
 	// Picklist
-	'PLL_UNASSIGNED' => 'Unassigned',
-	'PLL_AWAITING_REALIZATION' => 'Awaiting realization',
-	'PLL_FOR_PROCESSING' => 'For realization',
-	'PLL_IN_PROGRESSING' => 'In progress',
-	'PLL_SUBMITTED_COMMENTS' => 'Comments submitted',
-	'PLL_FOR_APPROVAL' => 'For approval',
-	'PLL_CANCELLED' => 'Cancelled',
-	'PLL_ACCEPTED' => 'Accepted',
+	'PLL_UNASSIGNED' => 'Attribué',
+	'PLL_AWAITING_REALIZATION' => 'En attente de réalisation',
+	'PLL_FOR_PROCESSING' => 'À réaliser',
+	'PLL_IN_PROGRESSING' => 'En cours',
+	'PLL_SUBMITTED_COMMENTS' => 'Commentaire envoyé',
+	'PLL_FOR_APPROVAL' => 'À approuver',
+	'PLL_CANCELLED' => 'Annulé',
+	'PLL_ACCEPTED' => 'Accepté',
 ];
