524d34dee9c8e91fda53d3eac2765b6daa49d78e
pklavia <pklavia@mail.com>
2017-02-10 17:37:46 +0200

    Translated into French

--- languages/fr_fr/CMileageLogbook.php
+++ languages/fr_fr/CMileageLogbook.php
@@ -1,20 +1,20 @@
 <?php
 /* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
 $languageStrings = [
-	'CMileageLogbook' => 'Mileage log book',
-	'SINGLE_CMileageLogbook' => 'Mileage log book',
+	'CMileageLogbook' => 'Livres de bord kilométrage',
+	'SINGLE_CMileageLogbook' => 'Livre de bord kilométrage',
 	//BLOCKS
-	'LBL_ADDITIONAL_INFORMATION' => 'Additional information',
-	'LBL_BASIC_INFORMATION' => 'Basic information',
+	'LBL_ADDITIONAL_INFORMATION' => 'Information supplémentaire',
+	'LBL_BASIC_INFORMATION' => 'Information de base',
 	'LBL_DESCRIPTION_BLOCK' => 'Description',
 	'LBL_ATTENTION_BLOCK' => 'Notes',
 	//FIELDS
-	'FL_SUBJECT' => 'Subject',
-	'FL_STATUS' => 'Status',
-	'FL_NUMBER_KILOMETERS' => 'Number of kilometers',
+	'FL_SUBJECT' => 'Sujet',
+	'FL_STATUS' => 'État',
+	'FL_NUMBER_KILOMETERS' => 'Nombre de kilomètres',
 	//PickList
-	'PLL_DRAFT' => 'Draft',
-	'PLL_FOR_APPROVAL' => 'For approval',
-	'PLL_ACCEPTED' => 'Accepted',
-	'PLL_CANCELLED' => 'Cancelled',
+	'PLL_DRAFT' => 'Brouillon',
+	'PLL_FOR_APPROVAL' => 'À approuver',
+	'PLL_ACCEPTED' => 'Accepté',
+	'PLL_CANCELLED' => 'Annulé',
 ];
